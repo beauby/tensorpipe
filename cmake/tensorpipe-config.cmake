2eae3b83f25e9a8f43f1a4097735e57aef393000
Lucas Hosseini <hoss@fb.com>
2020-04-21 16:07:19 +0200

    Fix tensorpipe CMake config.

--- cmake/tensorpipe-config.cmake
+++ cmake/tensorpipe-config.cmake
@@ -4,8 +4,8 @@
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
 
-get_filename_component(Tensorpipe_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+get_filename_component(tensorpipe_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
-if(NOT TARGET tensorpipe::libtensorpipe)
-    include("${Tensorpipe_CMAKE_DIR}/TensorpipeTargets.cmake")
+if(NOT TARGET tensorpipe::tensorpipe)
+    include("${tensorpipe_CMAKE_DIR}/tensorpipe-targets.cmake")
 endif()
